3c93afd3a66a475c1e6d465bffcea5c4e21e92ef
AbdElrahman33ml <12422022610559@pg.cu.edu.eg>
2025-02-02 00:57:28 +0200

    modify debug to true and accept all domain

--- project/settings.py
+++ project/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-t#3w%wzb694$tk0uqoc7isrehdm+f*dtg^7wjwiy@ykq&mztkh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
